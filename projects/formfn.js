ab586018f7c161e3f3c4d70b493536026611d3e5
Nivedha-s <60511981+Nivedha-s@users.noreply.github.com>
2020-03-31 21:58:16 +0530

    Add files via upload

---
+++ projects/formfn.js
@@ -0,0 +1,21 @@
+function Add_ed()
+{
+  document.getElementById("edq").innerHTML=document.getElementById("edq").innerHTML+document.getElementById("ed").innerHTML+document.getElementById("q").innerHTML;
+}
+
+function Add_we()
+{
+  document.getElementById("work").innerHTML=document.getElementById("work").innerHTML+document.getElementById("comp").innerHTML+document.getElementById("exp").innerHTML;
+}
+function Add_proj()
+{
+  document.getElementById("proj").innerHTML=document.getElementById("proj").innerHTML+document.getElementById("pt").innerHTML+document.getElementById("pd").innerHTML;
+}
+function Add_skill()
+{
+  document.getElementById("oskl").innerHTML=document.getElementById("oskl").innerHTML+document.getElementById("skll").innerHTML;}
+
+function Add_ach()
+{
+    document.getElementById("achm").innerHTML=document.getElementById("achm").innerHTML+document.getElementById('acht').innerHTML;
+}
